3a7ee12d9a2275bc0200556d481ca95563e338fc
Samuel-Harwood <samuelharwood3823@gmail.com>
2024-12-05 21:25:19 +0000

    Sanitised user input

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from converters import *
 
 
 def main():
-    user_input = input("Enter hex: ")
+    user_input = input("Enter hex: ").upper()
+    if user_input.startswith('0x'):
+        user_input = user_input[2:]
+    print(user_input)
+
     binary = hex_to_binary(user_input) #Convert to binary (we do this to make the conversion to denary easier)
     denary = binary_to_denary(binary) #Convert binary to denary 
     output = apply_positional_weighting(denary) #Combine all those values together to one value, using the formula i * (16^n-1)
@@ -14,11 +18,10 @@ def main():
     sum = 0
     for i in range(0, len(output)):
         sum += output[i]
-    print(sum)
 
-    
-    print(bin(sum))
-    print(oct(sum))
+    print(f'Denary:', sum)
+    print(f'Binary:',bin(sum)[2:])
+    print(f'Oct:',oct(sum)[2:])
 
 
 
